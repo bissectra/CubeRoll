2abdc56eee23757194ebc9c87f856d497ed68d83
Caio Costa <atsocs.cc@gmail.com>
2025-12-16 04:31:38 +0100

    multiple entries

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,14 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
   base: "/CubeRoll/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        stats: resolve(__dirname, "stats.html"),
+      },
+    },
+  },
 });
